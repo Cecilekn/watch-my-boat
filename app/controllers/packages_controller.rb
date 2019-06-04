6a9a19c99298e21a23afd62ae49889a85c9ae6d9
Celia Heidsieck <celia.heidsieck@gmail.com>
2019-06-04 15:19:07 +0200

    v1 de la show

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,7 +1,10 @@
 class PackagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
   end
 
   def show
+    @package = Package.find(params[:id])
+    @booking = @package.bookings.new
   end
 end
