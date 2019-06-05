591bc02334432e66b118c578d625167e9c7e74ab
Cecile Kwan Ning <cecile.kwanning@gmail.com>
2019-06-05 17:14:38 +0200

    Geocoding implemented

--- app/javascript/components/weather.js
+++ app/javascript/components/weather.js
@@ -3,11 +3,12 @@ const fetchByCoord = (lat, lon) => {
   const temp = document.querySelector("#weather-temp");
   const img = document.querySelector("#weather-img");
 
-  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f7e8f53ec526b533e8c9fa6a6a59885b&units=metric`)
+  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f7e8f53ec526b533e8c9fa6a6a59885b&units=metric&lang=fr`)
     .then(response => response.json())
     .then((data) => {
+      console.log(data)
       sky.innerText = `${data.weather[0].description}`;
-      temp.innerText = `${data.main.temp} °C`;
+      temp.innerText = `${Math.round(data.main.temp)} °C`;
       img.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     });
 };
