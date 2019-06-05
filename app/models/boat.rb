da2953ad685f3114518bb08a66d16cc2215fceaa
Celia Heidsieck <celia.heidsieck@gmail.com>
2019-06-05 17:41:54 +0200

    forms v2

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -5,6 +5,10 @@ class Boat < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :bookings, dependent: :destroy
 
-  validates :name, :address, :photo, :category, :size, presence: true
+  CATEGORIES = ["Voilier", "Moteur"]
+  SIZE = ["< 10m", "entre 10 et 20m", "> 20m"]
 
+  validates :name, :address, :photo, :category, :size, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :size, inclusion: { in: SIZE }
 end
