2444d7f2c94dc57362bae19ab93e26367fb512bc
Cecile Kwan Ning <cecile.kwanning@gmail.com>
2019-06-06 11:19:53 +0200

    Implement auto-complete on user and boat's address

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { fetchWeather } from '../components/weather';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 initUpdateNavbarOnScroll();
 fetchWeather();
+initAutocomplete();
