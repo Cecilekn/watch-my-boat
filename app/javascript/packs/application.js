0f9fba6743074ad17caaf4ddd7ba55767e502a97
Cecile Kwan Ning <cecile.kwanning@gmail.com>
2019-06-07 17:13:18 +0200

    Fix carroussel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,39 +5,13 @@ import { fetchWeather } from '../components/weather';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import "../plugins/flatpickr";
 import { mosaic } from '../components/mosaic';
-// import { moveToSelected } from '../components/carrousel';
+import { moveToSelected } from '../components/carroussel';
 
 initUpdateNavbarOnScroll();
 fetchWeather();
 initAutocomplete();
 mosaic();
-// moveToSelected();
 
-function moveToSelected(element) {
-  if (element == "next") {
-    var selected = $(".selected").next();
-  } else if (element == "prev") {
-    var selected = $(".selected").prev();
-  } else {
-    var selected = element;
-  }
-
-  var next = $(selected).next();
-  var prev = $(selected).prev();
-  var prevSecond = $(prev).prev();
-  var nextSecond = $(next).next();
-
-  $(selected).removeClass().addClass("selected");
-
-  $(prev).removeClass().addClass("prev");
-  $(next).removeClass().addClass("next");
-
-  $(nextSecond).removeClass().addClass("nextRightSecond");
-  $(prevSecond).removeClass().addClass("prevLeftSecond");
-
-  $(nextSecond).nextAll().removeClass().addClass('hideRight');
-  $(prevSecond).prevAll().removeClass().addClass('hideLeft');
-}
 
 // Eventos teclado
 $(document).keydown(function(e) {
@@ -67,4 +41,3 @@ $('#next').click(function() {
   moveToSelected('next');
 });
 
-// window.$ = $
